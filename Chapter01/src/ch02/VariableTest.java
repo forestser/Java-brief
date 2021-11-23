3c5d89323d06134e86eae3344e01af5af0bf5c52
forestser <forester00@gmail.com>
2021-11-23 10:54:33 +0900

    Chapter 1 내용 중 java 시작

---
+++ Chapter01/src/ch02/VariableTest.java
@@ -0,0 +1,14 @@
+package ch02;
+
+public class VariableTest {
+
+	public static void main(String[] args) {
+		int age, count;
+		age = 17;
+		System.out.println(age);
+		int level = 256;
+		System.out.println(level);
+
+	}
+
+}
---
+++ Chapter01/src/ch03/VariableTest.java
@@ -0,0 +1,15 @@
+package ch03;
+
+public class VariableTest {
+
+	public static void main(String[] args) {
+		byte bNum = 127;
+		System.out.println(bNum);
+		
+		//int iNum = 1234567890;
+		long lNum = 12345678900L;
+		System.out.println(lNum);
+
+	}
+
+}
